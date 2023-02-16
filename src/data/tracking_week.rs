8d27473302d9aa37c65fbb569cf0ed36d8ec7db5
Achsion <kevin1722000@gmail.com>
2023-02-16 03:12:51 +0100

    read and persist csv data

--- src/data/tracking_week.rs
+++ src/data/tracking_week.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 
 pub struct TrackingWeek {
     file_path: String,
+    records: Vec<DummyRecord>,
 }
 
 impl TrackingWeek {
@@ -11,29 +12,20 @@ impl TrackingWeek {
         let file_path = format!("{}/{}", env!("CARGO_MANIFEST_DIR"), file);
         let mut reader = Reader::from_path(file_path.clone())?;
 
-        //TODO: save result records in TrackingWeek
-        for result in reader.deserialize() {
-            let record: DummyRecord = result?;
-            println!("{:?}", record);
-        }
-
-        Ok(Self { file_path })
+        Ok(Self {
+            file_path,
+            records: Vec::from_iter(reader.deserialize().filter_map(|r| r.ok())),
+        })
     }
 
     pub fn save(&self) -> Result<(), csv::Error> {
         let mut writer = Writer::from_path(self.file_path.clone())?;
 
-        //TODO: saved all records in file
-        writer.serialize(DummyRecord {
-            name: String::from("Amogus"),
-            positive_number: 372,
-            negative_number: -5678,
-            float_number: 9.789,
-            description: Some(String::from("Sussy baka :3")),
-        })?;
+        for record in &self.records {
+            writer.serialize(record)?;
+        }
 
         writer.flush()?;
-
         Ok(())
     }
 }
